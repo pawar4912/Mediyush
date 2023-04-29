86e2f98a3b0cd91063c9b8644c9b96de9156f909
Tejaswini3131 <tejaswini9605@gmail.com>
2023-04-30 00:19:09 +0530

    add to courses

--- app/Http/Controllers/Portal/ServiceController.php
+++ app/Http/Controllers/Portal/ServiceController.php
@@ -8,6 +8,9 @@ use App\Models\Event;
 use App\Models\Product;
 use App\Models\GallaryPhoto;
 use App\Models\Course;
+use App\Models\cart;
+use Auth;
+use DB;
 
 class ServiceController extends Controller
 {
@@ -20,7 +23,12 @@ class ServiceController extends Controller
 	public function getCourse()
 	{
 		$course = Course::orderBy('id', 'DESC')->get();
-		return view('portal.services.course', compact('course'));
+		if(Auth::guard('user')->user()){
+			$user=Auth::guard('user')->user();
+			return view('portal.services.course',compact('user', 'course'));
+		} else {
+			return view('portal.services.course', compact('course'));
+		}
 	}
 
 	// public function getNews()
@@ -28,4 +36,49 @@ class ServiceController extends Controller
 	// 	$course = Course::orderBy('id', 'DESC')->get();
 	// 	return view('portal.services.course', compact('course'));
 	// }
+
+	public function addToCart($id, Request $req)
+	{
+		if(Auth::guard('user')->user()){
+			$user=Auth::guard('user')->user();
+
+			$logginUser = $user->id;
+			$courseId = $id;
+			$isAddedToCart = cart::where('userid', $logginUser)->where('courseid', $courseId)->get();
+
+			if ($isAddedToCart && count($isAddedToCart) !== 0) {
+				return back()->with('error','Course already in cart');
+			}
+
+			$cart = new cart();
+      $cart->userid = $logginUser;
+      $cart->courseid = $courseId;
+      $cart->save();
+			return redirect('cart');
+		} else {
+			return back()->with('error','Please login first');
+		}
+	}
+
+	public function getCart()
+	{
+		if(Auth::guard('user')->user()){
+			$user=Auth::guard('user')->user();
+			$logginUser = $user->id;
+			
+			$carts = DB::table('carts')
+            ->select('carts.id', 'courses.start_date', 'courses.end_date', 'courses.auther', 'courses.name', 'courses.banner')
+            ->join('users', 'carts.userid', '=', 'users.id')
+            ->join('courses', 'carts.courseid', '=', 'courses.id')
+						->where('carts.userid', $logginUser)
+						->get();
+			return view('portal.cart',compact('carts', 'user'));
+		}
+	}
+
+	public function deleteCart($id) {
+		$deleteCart = cart::find($id);
+		$deleteCart->delete();
+		return redirect('cart')->with('success','Course remove successfully!!');
+	}
 }
